4f44da779e768703833a5791003884d76c6c7fa2
Takuya Muakami <tmurakam@tmurakam.org>
2009-01-06 06:52:26 +0000

    add webapi

---
+++ Classes/webapi/WebApi.m
@@ -0,0 +1,104 @@
+// -*-  Mode:ObjC; c-basic-offset:4; tab-width:8; indent-tabs-mode:nil -*-
+/*
+  ItemShelf for iPhone/iPod touch
+
+  Copyright (c) 2008, ItemShelf Development Team. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  1. Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer. 
+
+  2. Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution. 
+
+  3. Neither the name of the project nor the names of its contributors
+  may be used to endorse or promote products derived from this software
+  without specific prior written permission. 
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import "WebApi.h"
+#import "DataModel.h"
+#import "URLComponent.h"
+#import "HttpClient.h"
+
+@implementation WebApi
+@synthesize delegate;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        delegate = nil;
+    }
+    return self;
+}
+
+/**
+   Set country code
+*/
+- (void)setCountry:(NSString*)country
+{
+    ASSERT(NO); // must be override
+}
+
+/**
+   Execute item search
+*/
+- (void)itemSearch
+{
+    ASSERT(NO); // must be override
+}
+
+/**
+   Start http request
+*/
+- (void)sendHttpRequest:(NSURL*)url
+{
+    HttpClient *httpClient = [[HttpClient alloc] init:self];
+    [httpClient requestGet:uri];
+    [httpClient release];
+}
+
+/**
+   @name HttpClientDelegate
+*/
+//@{
+
+- (void)httpClientDidFailed:(HttpClient*)client error:(NSError*)err
+{
+    // show error : TBD
+    if (delegate) {
+        [delegate webApiDidFailed:self reason:WEBAPI_ERROR_NETWORK message:nil];
+    }
+}
+
+// 読み込み完了時の処理
+- (void)httpClientDidFinish:(HttpClient*)client
+{
+    // You must override this method
+#if 0
+    NSString *response = [[[NSString alloc] initWithData:client.receivedData encoding:NSUTF8StringEncoding] autorelease];
+    LOG(@"response = %@", response);
+    //const char *utf8 = [response UTF8String];
+#endif
+}
+
+//@}
+
+@end
