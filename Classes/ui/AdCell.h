1667ae2c1e13bc66432c30d111c0825ae8960764
Takuya Murakami <tmurakam@tmurakam.org>
2010-03-11 20:24:59 +0900

    fix afma test

--- Classes/ui/AdCell.h
+++ Classes/ui/AdCell.h
@@ -11,7 +11,7 @@
 #define AFMA_CLIENT_ID  @"ca-mb-app-pub-4621925249922081"
 #define AFMA_CHANNEL_IDS @"6284604915"
 #define AFMA_KEYWORDS  @"本,書籍,コミック,CD,DVD,book,comics" // shopping
-#define AFMA_IS_TEST 1
+#define AFMA_IS_TEST 0
 
 @interface AdCell : UITableViewCell <GADAdViewControllerDelegate> {
     GADAdViewController *adViewController;
